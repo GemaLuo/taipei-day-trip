0e8e410af6571cad2bc5ab6d994234e0a2995688
GemaLuo <gemaluowenjun@gmail.com>
2022-11-23 16:05:44 +0800

    add category full text search

--- data/data.py
+++ data/data.py
@@ -8,14 +8,26 @@ mydb=mysql.connector.connect(
     password="password", #passwordforec
     auth_plugin='mysql_native_password',
     db="taipeitrip",
-    charset="utf8"
+    charset="utf8",
+    pool_name="mypool",
+    pool_size=3, 
 )
+connectionPool=mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=3,
+    pool_reset_session=True,
+    auth_plugin='mysql_native_password',
+    host="localhost",
+    db="taipeitrip",
+    user="root",
+    password="password"
+)
+mydb=connectionPool.get_connection()
+cur=mydb.cursor()
 
 file=open("D:\\taipei-day-trip\\data\\taipei-attractions.json", 'r', encoding='utf-8').read()
 json_obj=json.loads(file)
 data=json_obj["result"]["results"]
-
-cur=mydb.cursor()
 
 for item in data:
     id=item["_id"]
@@ -34,13 +46,11 @@ for item in data:
         if i.endswith(".jpg") or i.endswith(".JPG"):
             images.append("https"+i)
     images=str(images)
-    try:
-        sql="INSERT INTO attractions (id, name, category, description, address, transport, mrt, lat, lng, images) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        val=(id, name, category, description, address, transport, mrt, lat, lng, images)
-        cur.execute(sql, val)
-        mydb.commit()
-    except:
-        pass
+    sql="INSERT INTO attractions (id, name, category, description, address, transport, mrt, lat, lng, images) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val=(id, name, category, description, address, transport, mrt, lat, lng, images)
+    cur.execute(sql, val)
+    mydb.commit()
+mydb.close()
 
     
     
