de7a6180ea2b2f2d49ff8af49f2b453f08285260
GemaLuo <gemaluowenjun@gmail.com>
2022-11-23 16:03:12 +0800

    add category full text search

--- app.py
+++ app.py
@@ -2,20 +2,23 @@ from flask import *
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
-from flask import Flask, request, abort, jsonify
-import mysql.connector
+app.config["JSON_SORT_KEYS"]=False #固定
+
+from flask import Flask, jsonify
+import mysql.connector, math
 from mysql.connector.pooling import MySQLConnectionPool
 
 mydb_pool=mysql.connector.pooling.MySQLConnectionPool(
-	pool_name="mypool",
-	pool_size=3,
-    host="localhost",
-    user="root",
-    password="password",
-	auth_plugin='mysql_native_password',
-    db="taipeitrip",
-    charset="utf8",
-)
+		pool_name="mypool",
+		pool_size=3,
+    	host="localhost",
+    	user="root",
+    	password="password",
+		auth_plugin='mysql_native_password',
+    	db="taipeitrip",
+    	charset="utf8")
+# print(mydb_pool.pool_name)
+# print(mydb_pool.pool_size)
 
 # Pages
 @app.route("/")
@@ -35,83 +38,77 @@ def thankyou():
 #1st API 取得景點資料列表
 @app.route("/api/attractions")
 def attractions():
-	#讀取頁數
-	page=int(request.args.get("page", 0))
-	page_num=page*12
-	next_pagenum=(page+1)*12
-	#關鍵字搜尋
-	try:
-		db=mydb_pool.get_connection() 
-		cur=db.cursor(dictionary=True,buffered=True)
-		keyword=request.args.get("keyword", None)
-		sql_key="SELECT * FROM attractions WHERE name LIKE '%%%s%%' LIMIT %s, 12; " % (keyword, page_num,)
-		sql="SELECT * FROM attractions LIMIT %s, 12; " % (page_num,)
-		sql_next="SELECT name FROM attractions LIMIT %s, 12; " % (next_pagenum,)
-		sql_next_key="SELECT name FROM attractions WHERE name LIKE '%%%s%%' LIMIT %s, 12; " % (keyword, next_pagenum,)
+	page = int(request.args.get("page"))
+	pages=page*12
+	keyword = request.args.get("keyword")
 
-		#判斷keyword
-		if keyword != None:
-			cur.execute(sql_key)
-			result=cur.fetchall() #dict=true become dict
+	db=mydb_pool.get_connection()
+	cur=db.cursor(buffered=True)
 
-			#圖片處理-迴圈
-			for i in range(len(result)):
-				cur.execute(sql_key)
-				img_result=cur.fetchall()
-				img_list=img_result[i]["images"].split(",") #list
-				img_list=img_list[0:-1] #cuz last one is null
-				x=["images"]
-				y=[img_list]
-				img2list=dict(zip(x,y))
-				result[i].update(img2list)
-			z=["nextPage", "data"]
-			k=[page+1, result]
-			listplus=dict(zip(z,k))
+	#1-1(有keyword)搜尋category or name的資料，完全比對景點分類&模糊比對景點名稱
+	sql1="SELECT * FROM attractions WHERE category = %s OR LOCATE(%s, name)>0 LIMIT %s,%s"
+	val1=(keyword, keyword, pages, 12)
+	
+	#1-2(有keyword)category or name的資料筆數
+	count1="SELECT COUNT(*) FROM attractions WHERE category = %s OR LOCATE(%s, name)>0"
+	val2=(keyword, keyword)
 
-			#nextPage keyword
-			cur.execute(sql_next_key)
-			next_result=cur.fetchall()
+	#2-1(no keyword)all pages
+	sql2="SELECT * FROM attractions LIMIT %s,%s"
+	val3=(pages, 12)
 
-			if next_result==[]:  #fetch next page,if list is []/nothing,null
-				x=["nextPage", "data"]
-				y=[None, result]
-				result_next_null=dict(zip(x,y))
-				return result_next_null
-			return listplus
-
-		cur.execute(sql)
+	#2-2(no keyword)all pages的資料筆數
+	count3="SELECT count(*) FROM attractions"
+	
+	if keyword != None:
+		#1-1
+		cur.execute(sql1, val1)
 		result=cur.fetchall()
-
-		for i in range(len(result)):
-			cur.execute(sql)
-			img_result=cur.fetchall()
-			img_list=img_result[i]["images"].split(",")
-			img_list=img_list[0:-1]
-			x=["images"]
-			y=[img_list]
-			img2list=dict(zip(x,y))
-			result[i].update(img2list)
-
-		#combined "nextPage" & "data" with database
-		z=["nextPage", "data"]
-		k=[page+1, result]
-		listplus=dict(zip(z,k))
-
-		#make sure nextpage
-		cur.execute(sql_next)
-		next_result=cur.fetchall()
-
-		if next_result==[]:
-			z=["nextPage", "data"]
-			k=[None,result]
-			result_next_null=dict(zip(z,k))
-			return result_next_null
-		return listplus
+		#1-2
+		cur.execute(count1, val2)
+		num=cur.fetchone()[0]
+	else:
+		#2-1
+		cur.execute(sql2, val3)
+		result=cur.fetchall()
+		#2-2
+		cur.execute(count3)
+		num=cur.fetchone()[0]
+	#算最後一頁:用math.ceil(x)取大於or等於x值的整數
+	last=math.ceil(num/12)
+	next=page+1
+	if next>=last:
+		next=None
+	i=0 
+	mylist=[]
+	try:
+		while i<len(result): 
+			myresult = {				
+				"id" : result[i][0],
+				"name" : result[i][1],
+				"category" : result[i][2],
+				"description" : result[i][3],
+				"address" : result[i][4],
+				"transport" : result[i][5],
+				"mrt" : result[i][6],
+				"lat" : result[i][7],
+				"lng" : result[i][8],
+				#無法使用replace->eval(字串)可去掉字串的兩個引號
+				"images" : eval(result[i][9]) 
+			}
+			list.append(myresult)
+			i=i+1
+		return jsonify({
+			"nextPage" : next, 
+			"data" : mylist
+			})
 	except:
-		return abort(500)
+		return jsonify({
+			"error": True,
+			"message": "伺服器內部錯誤"
+		}), 500
 	finally:
 		db.close()
-
 
 #2nd API 根據景點編號取得景點資料
 @app.route("/api/attraction/<id>")
@@ -122,34 +119,28 @@ def attraction_id(id):
 		id_sql="SELECT * FROM attractions WHERE id=%s ;" % (id,)
 		cur.execute(id_sql)
 		result=cur.fetchall()
-		db.close()
-
-		if result==None or result=="" or id=="" or id=="id" or id==None:
-			return jsonify({"error":True, "message":"景點編號不正確"}), 400
-
-		img_list=result[0]["images"].split(",")
+		img_list=result[0]["images"].replace("[","").replace("]","").replace("'","").split(",")
 		img_list=img_list[0:-1]
 		x=["images"]
 		y=[img_list]
 		img2list=dict(zip(x,y))
 		result[0].update(img2list)
-
 		z=["data"]
 		k=result
 		listplus=dict(zip(z,k))
-		
 		return listplus
-	except:
-		return abort(500)
-
-@app.errorhandler(400)
-def request_failed(e):
-	return jsonify({"error":True, "message":"景點編號不正確"}), 400
-
-@app.errorhandler(500)
-def request_failed(e):
-	return jsonify({"error":True,"message":"伺服器內部錯誤"}), 500
-
+	except TypeError:
+		return jsonify({
+			"error":True,
+			"message":"景點編號不正確"
+		}), 400
+	except Exception:
+		return jsonify({
+			"error":True,
+			"message":"伺服器錯誤"
+		}), 500
+	finally:
+		db.close()
 
 #3rd API 取得景點分類名稱列表
 @app.route("/api/categories")
@@ -162,13 +153,18 @@ def category():
 		myresult=set(cur.fetchall())
 		categories=[]
 		for result in myresult:
-			obj=result[0].replace(u'\u3000', u'')
+			obj=result[0].replace(u'\u3000', u'') #去除空格:https://www.cnblogs.com/zqifa/p/python-9.html
 			categories.append(obj)
-		res_data={"data":categories}
-		return res_data
-	except:
-		return jsonify({"error":True,"message":"伺服器內部錯誤"})
+		get_data={"data":categories}
+		return get_data
+	except Exception:
+		return jsonify({
+			"error":True,
+			"message":"伺服器內部錯誤"
+		}), 500
+	finally:
+		db.close()
 
 	
-
-app.run(host="0.0.0.0", port=3000)
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=3000)
